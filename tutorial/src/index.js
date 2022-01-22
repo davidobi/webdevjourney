bd167ce8824de94dbe0b954884278cbcc6051f39
davidobi <dbiggestdave@yahoo.com>
2022-01-22 15:42:34 +0000

    Added Css

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,7 +1,35 @@
 import React from "react";
 import ReactDom from "react-dom";
-function Greeting(){
-  return <h4>This is David and this is my first component.</h4>
-} 
 
-ReactDom.render(<Greeting />,document.getElementById('root'));
+//CSS
+import './index.css';
+
+function BookList(){
+  return (
+   <section className='booklist'>
+     <Book />
+    <Book />
+    <Book />
+    <Book />
+   </section>
+  );
+}  
+  const Book = () => {
+    return (
+      <article className='book'>
+        <Image />
+        <Title />
+        <Author />
+
+      </article>
+    );
+  };
+
+  const Image = () => (<img src="https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL200_SR200,200_.jpg"
+  alt="" />);
+
+  const Title = () => (<h3>Reminders of Him: A Novel</h3>)
+  const Author = () => (<h1>Colleen Hoover</h1>)
+
+ 
+ReactDom.render(<BookList />,document.getElementById('root'));

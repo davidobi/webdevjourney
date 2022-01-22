c25b3c00b14072d8d18de9517b3ea5382affa87f
davidobi <dbiggestdave@yahoo.com>
2022-01-22 16:36:08 +0000

    adding props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,32 +4,46 @@ import ReactDom from "react-dom";
 //CSS
 import './index.css';
 
+
+//set up vars
+const firstBook = {
+img:"https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL200_SR200,200_.jpg",
+author: 'Colleen Hoover',
+title: 'Reminders of Him: A Novel'
+};
+
+const secondBook = {
+img:"https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL200_SR200,200_.jpg",
+author: 'Colleen Hoover',
+title: 'It Ends with Us: A Novel'
+};
+
 function BookList(){
   return (
    <section className='booklist'>
-     <Book />
-    <Book />
-    <Book />
-    <Book />
+     <Book 
+     img={firstBook.img} 
+     title={firstBook.title} 
+      author={firstBook.author} />
+    
+     <Book 
+     img={secondBook.img} 
+     title={secondBook.title} 
+      author={secondBook.author} />
+    
    </section>
   );
 }  
-  const Book = () => {
+  //const Book = ({img, title, author}) =>
+  const Book = (props) => {
+    const { img, title, author } = props;
     return (
       <article className='book'>
-        <Image />
-        <Title />
-        <Author />
-
+        <img src={img} alt="" />
+        <h1>{title}</h1>
+        <h4>{author}</h4>
       </article>
     );
   };
-
-  const Image = () => (<img src="https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL200_SR200,200_.jpg"
-  alt="" />);
-
-  const Title = () => (<h3>Reminders of Him: A Novel</h3>)
-  const Author = () => (<h1>Colleen Hoover</h1>)
-
  
 ReactDom.render(<BookList />,document.getElementById('root'));
